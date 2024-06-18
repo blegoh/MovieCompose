1675df5f6a23836af77c7a957dcab44f3ddabe41
Yusuf Eka Sayogana <yusufblegoh@gmail.com>
2024-06-18 13:53:18 +0700

    uodate movie detail

--- app/src/main/java/com/example/moviecompose/view/MovieDetail.kt
+++ app/src/main/java/com/example/moviecompose/view/MovieDetail.kt
@@ -1,5 +1,6 @@
 package com.example.moviecompose.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -8,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
@@ -21,11 +24,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import coil.compose.AsyncImage
+import com.example.moviecompose.R
 import com.example.moviecompose.theme.Blue1
 import com.example.moviecompose.theme.GenreBg
 import com.example.moviecompose.theme.GenreText
@@ -122,10 +128,54 @@ fun MovieDetail() {
                 shape = RoundedCornerShape(20.dp)
             )
         }
+        ConstraintLayout(
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .padding(top = 16.dp)
+                .fillMaxWidth()
+                .wrapContentHeight()
+        ) {
+            val (share) = createRefs()
+            val (download) = createRefs()
+            val (watchlist) = createRefs()
+            Image(
+                painter = painterResource(id = R.drawable.share),
+                contentDescription = "",
+                modifier = Modifier
+                    .constrainAs(share) {
+                        top.linkTo(parent.top)
+                        end.linkTo(parent.end)
+                    }
+                    .height(20.dp)
+                    .width(20.dp)
+            )
+            Image(
+                painter = painterResource(id = R.drawable.download),
+                contentDescription = "",
+                modifier = Modifier
+                    .constrainAs(download) {
+                        top.linkTo(parent.top)
+                        end.linkTo(share.start, margin = 32.dp)
+                    }
+                    .height(24.dp)
+                    .width(24.dp)
+            )
+            Image(
+                painter = painterResource(id = R.drawable.watchlist),
+                contentDescription = "",
+                modifier = Modifier
+                    .constrainAs(watchlist) {
+                        top.linkTo(parent.top)
+                        end.linkTo(download.start, margin = 32.dp)
+                    }
+                    .height(20.dp)
+                    .width(16.dp)
+            )
+        }
     }
 }
 
-@Preview()
+@Preview
 @Composable
 fun PreviewMovieDetail() {
     MovieComposeTheme(darkTheme = false) {
