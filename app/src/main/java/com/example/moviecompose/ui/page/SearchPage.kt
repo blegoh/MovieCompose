3c8d9507849e4d89603c3c6ee63347502aa3ba4e
Yusuf Eka Sayogana <yusufblegoh@gmail.com>
2024-07-04 07:04:24 +0700

    update

--- app/src/main/java/com/example/moviecompose/ui/page/SearchPage.kt
+++ app/src/main/java/com/example/moviecompose/ui/page/SearchPage.kt
@@ -5,13 +5,18 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,12 +27,16 @@ import com.example.moviecompose.ui.component.RecentSearch
 
 @Composable
 fun SearchPage() {
+    var text by remember { mutableStateOf("") }
     Column(Modifier.fillMaxWidth()) {
-        TextField(
-            value = "",
-            onValueChange = {},
+        OutlinedTextField(
+            value = text,
+            onValueChange = {
+                text = it
+            },
             modifier = Modifier
                 .padding(horizontal = 16.dp)
+                .padding(top = 24.dp)
                 .fillMaxWidth(),
             leadingIcon = {
                 Icon(
@@ -39,13 +48,16 @@ fun SearchPage() {
                     imageVector = Icons.Filled.Clear, contentDescription = "clear"
                 )
             },
-            shape = CircleShape
+            shape = CircleShape,
+            keyboardActions = KeyboardActions {
+
+            }
         )
         Spacer(modifier = Modifier.padding(top = 16.dp))
         Text(
             text = "Recent Searches",
             color = Gray3,
-            fontSize = 14.sp,
+            fontSize = 16.sp,
             modifier = Modifier.padding(start = 16.dp)
         )
 
